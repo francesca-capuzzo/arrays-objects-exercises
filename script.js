9c9c1639ada236a024853a36d403ba307aac9a8a
Francesca Capuzzo <f.capuzzo9966@gmail.com>
2022-01-24 22:00:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 //*******************ESERCIZIO 1******************/
 //SUM OF A RANGE:
 
-function range(start, end) {
-    let rangeArray = [];
-    for (let i = 0; i <= rangeArray.length; i++) {
+let rangeArray = [];
+let step;
+function range(start, end, step) {
+    for (let i = 1; i <= end; i += step) {
         const element = rangeArray[i];
         rangeArray.push(i);
     }
     return rangeArray;
 }
 
-console.log(range(1, 10));
+console.log(range(1, 10, 2));
+
+
+let total = 0;
+
+function sum(array) {
+    for (let i = 0; i < array.length; i++) {
+        total += rangeArray[i];
+    }
+    return total;
+}
+console.log(sum(rangeArray));
+
+//NB FUNZIONA con numeri positivi ma non negativi nello step
