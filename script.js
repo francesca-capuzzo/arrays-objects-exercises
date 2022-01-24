44060374fc508db82a1d2e5e39ba72ebd9954c9a
Francesca Capuzzo <f.capuzzo9966@gmail.com>
2022-01-24 18:46:36 +0100

    1

---
+++ script.js
@@ -0,0 +1,13 @@
+//*******************ESERCIZIO 1******************/
+//SUM OF A RANGE:
+
+function range(start, end) {
+    let rangeArray = [];
+    for (let i = 0; i <= rangeArray.length; i++) {
+        const element = rangeArray[i];
+        rangeArray.push(i);
+    }
+    return rangeArray;
+}
+
+console.log(range(1, 10));
